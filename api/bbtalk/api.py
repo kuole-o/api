829a1dc3261a6afda15bb0dc9e02b0fa3a3f4f76
guole <1327031013@qq.com>
2022-09-27 00:22:54 +0800

    update

--- api/bbtalk/api.py
+++ api/bbtalk/api.py
@@ -2,29 +2,31 @@
 # @Author    : GuoLe
 # @Date      : 2022/9/24 15:53
 
+from crypt import methods
 from email import header
 import json
 import os
 import requests
-from flask import Flask, jsonify, request
+from flask import Flask, request
 from http.server import BaseHTTPRequestHandler
 
 
 app = Flask(__name__)
+@app.route('/',methods=['GET',['POST']])
+def welcome():
+    return 'Hello World'
 
-@app.route('/bbtalk',methods=['GET'])
-def index():
+@app.route('/api',methods=['GET'])
+def get_data():
     skip = request.args.get('skip')
     limit = request.args.get('limit')
-    return jsonify({'skip':skip, 'limit':limit})
-
-def get_data():
+    if skip : 
+        url = 'https://leancloud.guole.fun/1.1/classes/content' + '?where=%7B%7D&limit=' + limit + '&skip=' + skip + '&order=-createdAt'
+    else :
+        url = 'https://leancloud.guole.fun/1.1/classes/content' + '?where=%7B%7D&limit=' + limit + '&order=-createdAt'
     data = []
     header = {'X-LC-Id':os.environ["LEANCLOUD_APPID"],'X-LC-Key':os.environ["LEANCLOUD_APPKEY"],'Content-Type':'text/html;charset=utf-8'}
-    if (skip == None) : 
-        url = 'https://leancloud.guole.fun/1.1/classes/content' + '?where=%7B%7D&limit=' + limit + '&order=-createdAt'
-    else :
-        url = 'https://leancloud.guole.fun/1.1/classes/content' + '?where=%7B%7D&limit=' + limit + '&skip=' + skip + '&order=-createdAt'
+    #header = {'X-LC-Id':'','X-LC-Key':'','Content-Type':'text/html;charset=utf-8'}
     response = requests.get(url,headers=header)
     data = response.content
     print(data);
@@ -43,7 +45,6 @@ class handler(BaseHTTPRequestHandler):
         self.wfile.write(json.dumps(data).encode('utf-8'))
         return 
 
-
 if __name__ == '__main__':
-    index()
-    app.run(host='0.0.0.0', port=5000, debug=True)
+#    app.run(port=5000,debug=True)
+    print(get_data())
